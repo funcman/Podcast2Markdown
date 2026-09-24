17b2e15ca57febed3f9d3b6752f13daf94da2c86
funcman <hyq1986@gmail.com>
2026-09-24 14:24:25 +0800

    feat: 支持在 .env 中配置 PORT

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['formidable', '@ffprobe-installer/ffprobe'],
   },
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
 };
 
 export default nextConfig;
